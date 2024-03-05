8db768c4213a45de5f1984119b6aec28bb3a980f
EncepDiana <sammtypyy013@gmail.com>
2024-03-05 10:02:15 +0700

    first commit

---
+++ src/samm/pkg12/rpl/pkg2/Samm12RpL2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samm.pkg12.rpl.pkg2;
+
+/**
+ *
+ * @author Yo_10
+ */
+public class Samm12RpL2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
